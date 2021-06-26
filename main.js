d1f1f36320379fe7147f7525645aa472988cfb1c
vancityraichandani <64709539+vancityraichandani@users.noreply.github.com>
2021-06-26 15:09:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,41 +9,40 @@ const cheerio = require("cheerio");
 const allMatchObj = require("./allmatch");
 const fs = require("fs");
 const path = require("path");
+
 inputArr = process.argv.slice(2);
 var url;
-// by default 2021 is on, you can change it.
-// const url = "https://www.espncricinfo.com/series/ipl-2021-1249214"; // ipl 2021
-// const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595"; // ipl 2020
-// const url = "https://www.espncricinfo.com/series/ipl-2019-1165643"; // ipl 2019
-// const url = "https://www.espncricinfo.com/series/ipl-2018-1131611"; // ipl 2018
-// const url = "https://www.espncricinfo.com/series/ipl-2017-1078425"; // ipl 2017
-// const url = "https://www.espncricinfo.com/series/ipl-2016-968923"; // ipl 2016
-// const url = "https://www.espncricinfo.com/series/pepsi-indian-premier-league-2015-791129"; // ipl 2015
-// const url = "https://www.espncricinfo.com/series/pepsi-indian-premier-league-2014-695871"; // ipl 2014
-// const url = "https://www.espncricinfo.com/series/indian-premier-league-2013-586733"; // ipl 2013
-// const url = "https://www.espncricinfo.com/series/indian-premier-league-2012-520932"; // ipl 2012
-// const url = "https://www.espncricinfo.com/series/indian-premier-league-2011-466304"; // ipl 2011
-// const url = "https://www.espncricinfo.com/series/indian-premier-league-2009-10-418064"; // ipl 2010
-// const url = "https://www.espncricinfo.com/series/indian-premier-league-2009-374163"; // ipl 2009
-// const url = "https://www.espncricinfo.com/series/indian-premier-league-2007-08-313494"; // ipl 2008
 
 switch (inputArr[0]) {
 
     case "2020": url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595"; // ipl 2020 
+        break;
     case "2019": url = "https://www.espncricinfo.com/series/ipl-2019-1165643"; // ipl 2019
+        break;
     case "2018": url = "https://www.espncricinfo.com/series/ipl-2018-1131611"; // ipl 2018
+        break;
     case "2017": url = "https://www.espncricinfo.com/series/ipl-2017-1078425"; // ipl 2017
+        break;
     case "2016": url = "https://www.espncricinfo.com/series/ipl-2016-968923"; // ipl 2016
+        break;
     case "2015": url = "https://www.espncricinfo.com/series/pepsi-indian-premier-league-2015-791129"; // ipl 2015
+        break;
     case "2014": url = "https://www.espncricinfo.com/series/pepsi-indian-premier-league-2014-695871"; // ipl 2014
+        break;
     case "2013": url = "https://www.espncricinfo.com/series/indian-premier-league-2013-586733"; // ipl 2013
+        break;
     case "2012": url = "https://www.espncricinfo.com/series/indian-premier-league-2012-520932"; // ipl 2012
+        break;
     case "2011": url = "https://www.espncricinfo.com/series/indian-premier-league-2011-466304"; // ipl 2011
+        break;
     case "2010": url = "https://www.espncricinfo.com/series/indian-premier-league-2009-10-418064"; // ipl 2010
+        break;
     case "2009": url = "https://www.espncricinfo.com/series/indian-premier-league-2009-374163"; // ipl 2009
+        break;
     case "2008": url = "https://www.espncricinfo.com/series/indian-premier-league-2007-08-313494"; //ipl 2008
+        break;
     default: url = "https://www.espncricinfo.com/series/ipl-2021-1249214"; // ipl 2021
-                    inputArr[0] = "2021";
+        inputArr[0] = "2021";
 
 }
 
