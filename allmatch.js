05468f615c5255f4dbe2f469295ae34fdc6bff13
vancityraichandani <64709539+vancityraichandani@users.noreply.github.com>
2021-06-26 14:50:12 +0530

    Update allmatch.js

--- allmatch.js
+++ allmatch.js
@@ -2,29 +2,30 @@ const request = require("request");
 const cheerio = require("cheerio");
 const scorecardObj = require("./scorecard");
 
-function getAllMatchesLink(url) {
+function getAllMatchesLink(url, folderName) {
     request(url, function (err, response, html) {
-        if(err){
+        if (err) {
             console.log(err);
-        }else{
+        } else {
             extractAllLinks(html);
         }
     })
-}
 
-function extractAllLinks(html){
-    let $ = cheerio.load(html);
-    let scorecardElems = $("a[data-hover='Scorecard']");
-    for(let i = 0; i < scorecardElems.length; i++){
-        let link = $(scorecardElems[i]).attr("href");
-        let flink = "https://www.espncricinfo.com" + link;
-        // console.log(flink);
-        scorecardObj.ps(flink);
+
+    function extractAllLinks(html) {
+        let $ = cheerio.load(html);
+        let scorecardElems = $("a[data-hover='Scorecard']");
+        for (let i = 0; i < scorecardElems.length; i++) {
+            let link = $(scorecardElems[i]).attr("href");
+            let flink = "https://www.espncricinfo.com" + link;
+            // console.log(flink);
+            scorecardObj.ps(flink, folderName);
+        }
     }
 }
 
 module.exports = {
-    getAllMatch : getAllMatchesLink
+    getAllMatch: getAllMatchesLink
 }
 
 // extracts all matches links and loops scorecard.js for different matches
